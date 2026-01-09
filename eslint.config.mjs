afe4649a3461c51a0a0e169caeefacc3199191bd
Muhammad Usman <muhammadusmanramzan586@gmail.com>
2026-01-09 07:37:18 +0500

    add eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+import eslint from '@eslint/js';
+import { defineConfig } from 'eslint/config';
+import tseslint from 'typescript-eslint';
+
+export default defineConfig(
+  {
+    ignores: ['dist/', 'node_modules/', 'eslint.config.mjs'],
+  },
+  eslint.configs.recommended,
+  tseslint.configs.recommendedTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname
+      },
+    },
+    rules: {
+        // "no-console": "warn",
+    }
+  },
+);
--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,15 @@
-console.log('Server is running ')
+function Welcome(name: string ) : string {
+    return `Welcome, ${name}!`;
+}
+
+console.log(Welcome("User"));
+
+
+const user = {
+    name : "Usman",
+    age: 25,
+    emplloyed: true
+}
+
+const userName = user.name;
+console.log(`User Name is : ${userName}`);
